8f8f1ea74a4ac069edfbb37b9ed7b32195e2a9f7
ABC <b01511398@gmail.com>
2024-05-24 10:05:15 +0800

    00000

--- src/main/java/com/filter/EmpFilter.java
+++ src/main/java/com/filter/EmpFilter.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 @Component
 public class EmpFilter implements HandlerInterceptor {
-    String[] ignoreUrls = {"/emp/toLogin","/emp/doLogin","/emp/resetPasswordLink","/front_end/**",
+    String[] ignoreUrls = {"/emp/toLogin","/emp/doLogin","/emp/resetPasswordLink","/front_end/**", "/mem/**", "/rental/**", 
             "/**/*.js", "/**/*.css", "/**/*.jpg", "/**/*.jpeg", "/**/*.png", "/**/*.gif", "/**/*.svg","/**/*.ico"};
     private AntPathMatcher antPathMatcher = new AntPathMatcher();
     @Override
