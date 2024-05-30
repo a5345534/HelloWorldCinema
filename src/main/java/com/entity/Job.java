293844b4378de29c61eb5fe1cdea2dda323911ff
喬喬 <juliechu311@gmail.com>
2024-05-30 16:39:14 +0800

    5/30更新

--- src/main/java/com/entity/Job.java
+++ src/main/java/com/entity/Job.java
@@ -3,6 +3,8 @@ package com.entity;
 import org.hibernate.annotations.GeneratorType;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,9 +14,13 @@ public class Job {
 
     @Id
     @Column(name = "job_id",nullable = false)
+    @NotEmpty(message="請勿空白")
+    @Pattern(regexp = "^[(0-9)]$", message = "員工職位: 只能是數字")
     private Integer jobId;
 
     @Column(name = "title",length = 4)
+    @NotEmpty(message="請勿空白")
+    @Pattern(regexp = "^[(\u4e00-\u9fa5)(a-zA-Z0-9_)]{1,4}$", message = "員工姓名: 只能是中、英文字母、數字和_ , 且長度必需在1到4之間")
     private String title;
     @OneToMany(cascade=CascadeType.REMOVE, fetch=FetchType.EAGER, mappedBy="job")
     private Set<Emp> emps = new HashSet<Emp>();
